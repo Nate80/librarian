dbdf546f25eeb9b9bc24a816826b2db1c309e6b6
Nathan Mohr <nlm9380@gmail.com>
2017-01-06 19:49:44 -0600

    Added Abstract Controller and comments explaining functionality

--- src/main/java/org/launchcode/librarian/controllers/AbstractController.java
+++ src/main/java/org/launchcode/librarian/controllers/AbstractController.java
@@ -8,12 +8,28 @@ import org.launchcode.librarian.models.dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbstractController {
+		
+	//allows us to query the database for user objects and entry objects
 	
 	@Autowired
 	protected UserDao userDao;
 	
 	@Autowired
 	protected entryDao entryDao;
-	
 
-}
+	//sets a universal key that can be used to determine whether a user is logged in
+	static final String userSessionKey = "user_id";
+
+		//retrieves a user currently in session
+	    protected User getUserFromSession(HttpSession session) {  
+	    	
+	        Integer userId = (Integer) session.getAttribute(userSessionKey);
+	        return userId == null ? null : userDao.findByUid(userId);
+	    }
+	    
+	    //used to set a user in a session
+	    protected void setUserInSession(HttpSession session, User user) {  
+	    	session.setAttribute(userSessionKey, user.getUid());
+	    }
+		
+	}
