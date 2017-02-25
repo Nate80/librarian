372d1d29614191e7518c9c79c95278d81891d03d
Nathan Mohr <nlm9380@gmail.com>
2017-02-24 19:23:29 -0600

    added a delete by UID parameter

--- src/main/java/org/launchcode/librarian/models/dao/EntryDao.java
+++ src/main/java/org/launchcode/librarian/models/dao/EntryDao.java
@@ -14,11 +14,15 @@ import org.springframework.stereotype.Repository;
 public interface EntryDao extends CrudRepository<Entry, Integer> {
     
     public List<Entry> findByAuthor(int authorId);
+    //public List<Entry> findByGenre(String genre);
     
     // allows you to search by various parameters
     public Entry findByUid(int uid);
     public Entry findByTitle(String title);
-    public Entry findByGenre(String genre);
+    
     public Entry findByCreator(String creator);
+    
+    int deleteByUid(int uid);
+ }
 	
-}
+
